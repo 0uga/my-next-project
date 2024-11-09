f117a7d835ed32883be963e802ceebd086709a0d
0uga <ktc24a31c0002@edu.kyoto-tech.ac.jp>
2024-11-09 15:45:37 +0900

    change

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   //JavaScriptを書いたり、参照する際には{}で囲む
   return (
     <div>
-      <h1 className={styles.title}>テクノロジーの力で{name}世界を変える</h1>
+      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
       <p className={styles.description}>
         私たちは市場をリードしているグローバルテックカンパニーです。
       </p>
