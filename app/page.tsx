2e2518df49ac18956d10c327fab9fcaddf80a84e
0uga <ktc24a31c0002@edu.kyoto-tech.ac.jp>
2024-11-09 15:35:57 +0900

    comit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <h1>テクノロジーの力で世界を変える</h1>;
+  // JavaScriptの定数や式などを書くことができる
+  const name = "世界";
+  
+  // HTMLのようなマークアップを書くことができる
+  //JavaScriptを書いたり、参照する際には{}で囲む
+  return (
+    <div>
+      <h1 className={styles.title}>テクノロジーの力で{name}世界を変える</h1>
+      <p className={styles.description}>
+        私たちは市場をリードしているグローバルテックカンパニーです。
+      </p>
+    </div>
+    );
 }
